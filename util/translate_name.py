13a2fa6f3ef8dfe7818f94a613236ac6c038ed9d
Henrich Hegedus <henri.hegedus@gmail.com>
2021-12-13 16:51:09 +0000

    add translate names

---
+++ util/translate_name.py
@@ -0,0 +1,35 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+
+from bs4 import BeautifulSoup as bs
+
+def get_clean_name(name):
+    """
+    Get a translation of team or player name based on a Google search of that name
+    :param name: word to search on Google
+    :return: tuple (english_name,player/team)
+    """
+    options = Options()
+    options.headless = False
+    options.incognito = True
+    browser = Firefox(options=options)
+
+    name = name.replace(" ",'+')
+    browser.get(f'https://www.google.com/search?q={name}')
+
+    soup = bs(browser.page_source, features="lxml")
+
+    browser.close()
+    entry = soup.find("span", class_ = 'tsp-ht') # class for player
+    if entry == None:
+        try:
+            entry = soup.find("div", class_ = 'ofy7ae') # class for team
+            return entry.text, "team"
+        except:
+            return None, None
+
+    else:
+        return entry.text, "player"
+
+
+    return None, None
